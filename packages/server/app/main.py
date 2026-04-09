0cb1e7e1f03a93f29d06ae26beb0c06453ea1064
Fresh Topia <karateboy105@gmail.com>
2026-04-08 19:19:06 -0500

    Restrict CORS to explicit methods and headers

--- packages/server/app/main.py
+++ packages/server/app/main.py
@@ -102,8 +102,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS.split(","),
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type", "X-CSRF-Token"],
 )
 
 # Starlette add_middleware prepends: last added = outermost.
